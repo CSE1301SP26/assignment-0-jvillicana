fe0c03b2bd2e19048b76720a2dd68f6cd51969c3
Jose <j.m.villicanabrugada@wustl.edu>
2026-01-14 15:09:05 -0600

    Assignment 0 Finished

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,87 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
-		robot.turnLeft(90);
-		robot.forward(23);
-		robot.turnRight(90);
-		robot.forward(80);
+	robot.forward(100);
+
+	robot.forward(30);
+	robot.turnLeft(90);
+	robot.forward(15);
+	robot.turnLeft(90);
+	robot.forward(30);
+	robot.turnLeft(90);
+	robot.forward(15);
+	robot.turnLeft(90);
+	robot.turnLeft(45);
+
+	robot.forward(30);
+	robot.turnLeft(90);
+	robot.forward(15);
+	robot.turnLeft(90);
+	robot.forward(30);
+	robot.turnLeft(90);
+	robot.forward(15);
+	robot.turnLeft(90);
+	robot.turnLeft(45);
+
+	robot.forward(30);
+	robot.turnLeft(90);
+	robot.forward(15);
+	robot.turnLeft(90);
+	robot.forward(30);
+	robot.turnLeft(90);
+	robot.forward(15);
+	robot.turnLeft(90);
+	robot.turnLeft(45);
+
+	robot.forward(30);
+	robot.turnLeft(90);
+	robot.forward(15);
+	robot.turnLeft(90);
+	robot.forward(30);
+	robot.turnLeft(90);
+	robot.forward(15);
+	robot.turnLeft(90);
+	robot.turnLeft(45);
+
+	robot.forward(30);
+	robot.turnLeft(90);
+	robot.forward(15);
+	robot.turnLeft(90);
+	robot.forward(30);
+	robot.turnLeft(90);
+	robot.forward(15);
+	robot.turnLeft(90);
+	robot.turnLeft(45);
+
+	robot.forward(30);
+	robot.turnLeft(90);
+	robot.forward(15);
+	robot.turnLeft(90);
+	robot.forward(30);
+	robot.turnLeft(90);
+	robot.forward(15);
+	robot.turnLeft(90);
+	robot.turnLeft(45);
+
+	robot.forward(30);
+	robot.turnLeft(90);
+	robot.forward(15);
+	robot.turnLeft(90);
+	robot.forward(30);
+	robot.turnLeft(90);
+	robot.forward(15);
+	robot.turnLeft(90);
+	robot.turnLeft(45);
+
+	robot.forward(30);
+	robot.turnLeft(90);
+	robot.forward(15);
+	robot.turnLeft(90);
+	robot.forward(30);
+	robot.turnLeft(90);
+	robot.forward(15);
+	robot.turnLeft(90);
+	robot.turnLeft(45);
 	}
 
 }
